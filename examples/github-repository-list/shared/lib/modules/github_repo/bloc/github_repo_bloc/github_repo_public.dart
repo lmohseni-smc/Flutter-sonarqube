85d8af6f4e4d08141a7cd488d15c805e16c83906
Logan Mohseni <mohsenil85@gmail.com>
2021-12-01 10:04:45 -0700

    use default flutter skeleton

--- examples/github-repository-list/shared/lib/modules/github_repo/bloc/github_repo_bloc/github_repo_public.dart
+++
@@ -1,3 +0,0 @@
-export 'package:shared/modules/github_repo/bloc/github_repo_bloc/github_repo_event.dart';
-export 'package:shared/modules/github_repo/bloc/github_repo_bloc/github_repo_state.dart';
-export 'package:shared/modules/github_repo/bloc/github_repo_bloc/github_repo_bloc.dart';
